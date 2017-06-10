4b74e04616d180ece036dfe5d4f86d9c7402704b
Twinity <onlyluckcze@gmail.com>
2017-06-10 21:58:34 +0200

    REACT TOOLS!

--- imports/ui/Menu.jsx
+++ imports/ui/Menu.jsx
@@ -19,14 +19,13 @@ class Menu extends Component {
 
     render() {
         return (
-            <div>
-                <AppBar leftIcon='menu'>
-                    <Navigation type='horizontal'>
-                        <Link href='http://' label='Inbox' icon='inbox' />
-                        <Link href='http://' active label='Profile' icon='person' />
-                    </Navigation>
-                </AppBar>
-            </div>
+
+        <AppBar title="React Toolbox" leftIcon="menu">
+            <Navigation type="horizontal">
+                <Link href="http://" label="Inbox" icon="inbox" />
+                <Link href="http://" active label="Profile" icon="person" />
+            </Navigation>
+        </AppBar>
         )
     }
 }
