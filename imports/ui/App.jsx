4c7e0376fe555dd765fdec5fa737ba2774c8f8b6
Twinity <onlyluckcze@gmail.com>
2017-06-10 19:19:32 +0200

    stuff

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -3,17 +3,21 @@ import { createContainer } from 'meteor/react-meteor-data';
 import { BrowserRouter, Route } from 'react-router-dom'
 import { Switch } from 'react-router';
 
+import Menu from './Menu'
 import CategoryList from './CategoryList';
 
 class App extends Component {
     render() {
         return (
-            <BrowserRouter>
-                <Switch>
-                    <Route path="/" component={CategoryList}/>
-                    <Route path="/detail" component={CategoryList}/>
-                </Switch>
-            </BrowserRouter>
+            <div>
+                <Menu/>
+                <BrowserRouter>
+                    <Switch>
+                        <Route path="/" component={CategoryList}/>
+                        <Route path="/detail" component={CategoryList}/>
+                    </Switch>
+                </BrowserRouter>
+            </div>
         )
     }
 }
---
+++ imports/ui/Menu.jsx
@@ -0,0 +1,19 @@
+import React, {Component, PropTypes} from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { BrowserRouter, Route } from 'react-router-dom'
+import { Switch } from 'react-router';
+
+class Menu extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default createContainer(() => {
+    return {
+    };
+}, Menu);
